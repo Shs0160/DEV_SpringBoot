858087ef3503c4b8bad6ac7b63d7199e246748ac
Shs0160 <aladinsue@gmail.com>
2025-03-28 12:05:23 +0900

    git init

---
+++ src/main/java/io/shs0160/greppapp/GreppAppApplication.java
@@ -0,0 +1,13 @@
+package io.shs0160.greppapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreppAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreppAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/shs0160/greppapp/GreppAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shs0160.greppapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreppAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
